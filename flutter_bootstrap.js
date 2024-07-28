c08009fb782a3e9765461da518640e6b2ac3989e
austen <anovis@ymail.com>
2024-07-28 17:24:03 -0400

    1.2.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1542161022"
+    serviceWorkerVersion: "942623504"
   }
 });
